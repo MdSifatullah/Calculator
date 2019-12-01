a7959468e69b2959612addd8a1ebc3d2a7d52809
Md Sifat <cse.sifat@gmail.com>
2019-12-01 15:39:39 +0600

    Operational work is undone

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firstElement: String = ""
+    var firstValue = ""
+    var secondValue = ""
+    var result = 0.0
 
+    @IBOutlet weak var ResultShowLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
+    @IBAction func ButtonPressedValue(_ sender: Any) {
+        
+        let tag = (sender as! UIButton).tag
+        
+        firstElement = firstElement + String(tag)
 
+                   ResultShowLbl.text = firstElement
+        
+    }
+    
+    @IBAction func Equal(_ sender: Any) {
+        
+        secondValue = firstElement
+         ResultShowLbl.text = firstValue + secondValue
+        
+    }
+    
+    @IBAction func Operation(_ sender: Any) {
+        
+        firstValue = firstElement
+        firstElement = ""
+        let tag = (sender as! UIButton).tag
+        if tag == 13{
+          
+            
+           
+//        result = firstValue
+            
+            ResultShowLbl.text = firstElement
+        }
+        
+    }
+
+    @IBAction func ClearAll(_ sender: Any) {
+     firstElement = ""
+        ResultShowLbl.text = "0"
+        firstValue = ""
+        secondValue = ""
+    }
 }
 
